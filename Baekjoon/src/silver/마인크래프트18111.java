7a6ab50fb6eb79fa97b4ab1f01f6f46037c6b933
Rawoon-Lee <rawoon@kakao.com>
2022-03-31 01:39:32 +0900

    3월 30일

---
+++ Baekjoon/src/silver/마인크래프트18111.java
@@ -0,0 +1,67 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 마인크래프트18111 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		int N = Integer.parseInt(str[0]);
+		int M = Integer.parseInt(str[1]);
+		int B = Integer.parseInt(str[2]);
+		int[][] house = new int[N][M];
+		// 전체 필드에서 최대값과 최솟값을 찾는다.
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			String[] tmp = br.readLine().split(" ");
+			for (int j = 0; j < M; j++) {
+				house[i][j] = Integer.parseInt(tmp[j]);
+				max = Math.max(max, house[i][j]);
+				min = Math.min(min, house[i][j]);
+			}
+		} // 입력끝
+
+		// 주의
+		// 답이 여러 개 있다면 그중에서 땅의 높이가 가장 높은 것을 출력하시오.
+
+		// 만들고 싶은 높이
+		int time = Integer.MAX_VALUE;
+		int height = 0;
+		outer: for (int h = max; h >= min; h--) {
+			// 내가 만들고 싶은 높이를 위해 들은 시간
+			int sum = 0;
+			// 내가 인벤토리에 가지고 있는 블럭의 수
+			int block = B;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					// 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다. :2초
+					if (house[i][j] > h) {
+						sum += 2 * (house[i][j] - h);
+						block += house[i][j] - h;
+					}
+					// 내가 만들고 싶은 층보다 더 작을 때
+					// 인벤토리에서 블록 하나를 꺼내어 좌표 (i, j)의 가장 위에 있는 블록 위에 놓는다. :1초
+					else if (house[i][j] < h) {
+						sum += h - house[i][j];
+						block -= h - house[i][j];
+					}
+				}
+			} // 전체 필드를 다 돌음
+			// block 이 0보다 작다면 무사히 평탄화를 성공할 만큼 충분한 블럭이 었었다는것
+			if (block < 0)
+				continue outer;
+			if (sum <= time) {
+				time = sum;
+				if (sum == time) {
+					height = Math.max(height, h);
+				} else {
+					height = h;
+				}
+			}
+		} // 답구하기 끝
+		System.out.printf("%d %d", time, height);
+	}
+}
--- Baekjoon/src/silver/피보나치함수1003.java
+++ Baekjoon/src/silver/피보나치함수1003.java
@@ -3,28 +3,19 @@ package silver;
 import java.util.Scanner;
 
 public class 피보나치함수1003 {
-	static int zero ;
-	static int one;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int i=0; i<T; i++) {
-			zero = 0;
-			one = 0;
-			fibo(sc.nextInt());
+			int zero = 0;
+			int one = 0;
+			int n = sc.nextInt();
+			for (int f=n; f>=0; f--) {
+				
+			}
+			
+			
 			System.out.printf("%d %d\n", zero, one);
 		}
-	}
-	static void fibo(int num) {
-		if (num == 0) {
-			zero++;
-			return;
-		}
-		if (num == 1) {
-			one++;
-			return;
-		}
-		fibo(num-1);
-		fibo(num-2);
 	}
 }
