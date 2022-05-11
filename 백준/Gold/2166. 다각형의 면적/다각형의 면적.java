7e336c82b93aa1a07f41c380a1396d23f80123f1
Rawoon-Lee <90673297+Rawoon-Lee@users.noreply.github.com>
2022-05-11 22:41:59 +0900

    [Gold V] Title: 다각형의 면적, Time: 376 ms, Memory: 34920 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		long[][] field = new long[N+1][2];
+		for (int i=0; i<N; i++) {
+			field[i][0] = sc.nextInt();
+			field[i][1] = sc.nextInt();
+		}
+		field[N] = field[0];
+		double sumA = 0;
+		double sumB = 0;
+		for (int i=0; i<N; i++) {
+			sumA += field[i][0] * field[i+1][1];
+			sumB += field[i][1] * field[i+1][0];
+//			System.out.println(ans);
+		}
+		System.out.printf("%.1f",Math.abs(sumA - sumB) / 2);
+	}
+}
