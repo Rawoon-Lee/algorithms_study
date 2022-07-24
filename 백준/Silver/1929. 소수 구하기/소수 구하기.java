fd750eb624710acd722d188f230365772843eb3e
Rawoon-Lee <90673297+Rawoon-Lee@users.noreply.github.com>
2022-07-25 00:36:43 +0900

    [Silver III] Title: 소수 구하기, Time: 196 ms, Memory: 18872 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		int M = Integer.parseInt(str[0]);
+		int N = Integer.parseInt(str[1]);
+		StringBuilder sb = new StringBuilder();
+		boolean[] prime = new boolean[N+1];
+		prime[0] = true;
+		prime[1] = true;
+		for (int i=2; i<=Math.sqrt(N); i++) {
+			if (prime[i]) continue;
+			for (int j= i*i; j<=N; j+=i) {
+				prime[j] = true;
+			}
+		}
+		for (int i = M; i <= N; i++) {
+			if (!prime[i])
+				sb.append(i).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
