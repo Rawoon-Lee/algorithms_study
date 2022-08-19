a0d4d20b8c273910a457dc36744483862d3c9b04
Rawoon-Lee <90673297+Rawoon-Lee@users.noreply.github.com>
2022-08-19 23:27:33 +0900

    [Silver V] Title: 나이순 정렬, Time: 292 ms, Memory: 60828 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,11 +1,9 @@
 import sys
-
 input = sys.stdin.readline
+
 N = int(input())
-members = []
-for _ in range(N):
-    age, name = map(str, input().split())
-    members.append(((int(age)), name))
-ans = sorted(members, key=lambda mem: mem[0])
-for i in range(N):
-    print(ans[i][0], ans[i][1])
+members = [input().split() for _ in range(N)]
+members.sort(key = lambda x: int(x[0]))
+
+for mem in members:
+    print(*mem)
