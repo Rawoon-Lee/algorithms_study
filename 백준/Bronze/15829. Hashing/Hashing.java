e926394eb23d1530a0192b6f0d5e4b9946dddcb1
Rawoon-Lee <90673297+Rawoon-Lee@users.noreply.github.com>
2022-08-23 23:36:56 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 12912 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		long hash = 0;
+		String str = sc.next();
+		long pow = (long) Math.pow(31, 0);
+		for (int i = 0; i < len; i++) {
+			hash += pow * (str.charAt(i) - '`');
+			pow = pow*31%1234567891;
+		}
+		hash %= 1234567891;
+		System.out.println(hash);
+	}
+}
