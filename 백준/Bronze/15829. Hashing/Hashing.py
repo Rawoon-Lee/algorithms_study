182aec6a19c551997f33f3d3d2aeb1b2830d53eb
Rawoon-Lee <90673297+Rawoon-Lee@users.noreply.github.com>
2022-08-23 23:48:02 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,15 +1,8 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+s = input()
+r, m = 31, 1234567891
 
-N = int(input())
-str = list(input())
-nums = [ord(x) - 96 for x in str]
-
-ans = 0
-pow = 1
-for i in range(N):
-    tmp = nums[i] * pow % 1234567891
-    ans += tmp
-    pow = pow * 31 % 1234567891
-
-print(int(ans % 1234567891))
+hash = 0
+for i, x in enumerate(s):
+    hash += (ord(x) - ord('a') + 1) * r ** i
+print(hash % m)
