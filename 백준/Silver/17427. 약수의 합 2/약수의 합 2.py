11c2547d88b8ff4cc0b78ea37846ee7bd241d207
Rawoon-Lee <90673297+Rawoon-Lee@users.noreply.github.com>
2023-01-25 16:26:36 +0900

    [Silver II] Title: 약수의 합 2, Time: 132 ms, Memory: 70432 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
@@ -1,12 +1,4 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
 
-N = int(input().strip())
-# answer variable
-ans = 0
-# except 1 from N nums
-for i in range(2, N+1):
-    # 배수의 개수
-    ans += N // i * i
-# 모두 1을 약수로 가지니까 N 더하기
-print(ans + N)
+ans = sum([N // i * i for i in range(1, N + 1)])
+print(ans)
